a2b52d91a515e0cfea39f4705c2bfff04ee85a6f
Petr Pavlín <pe.pavlin@gmail.com>
2026-01-25 09:25:16 +0100

    Use UrlState for smartSearch toggle

--- src/app/components/components/analytics/analytics.types.ts
+++ src/app/components/components/analytics/analytics.types.ts
@@ -3,6 +3,7 @@ import { VariantPackGuid } from '@/api/dtos'
 export const AnalyticsTrackEventNames: Record<AnalyticsEventNameType, string> =
 	{
 		VISIT_SONG: 'VISIT_SONG',
+		SMART_SEARCH_TOGGLE: 'SMART_SEARCH_TOGGLE',
 	}
 
 export type AnalyticsTrackData = {
@@ -11,6 +12,9 @@ export type AnalyticsTrackData = {
 		packGuid: VariantPackGuid
 		title: string
 		hasChords: boolean
+	}
+	SMART_SEARCH_TOGGLE: {
+		enabled: boolean
 	}
 }
 
